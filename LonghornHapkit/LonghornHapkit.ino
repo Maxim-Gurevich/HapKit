fe8bf9b86d3ad8603fc768c061d385a5ddac2322
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-09-27 16:20:12 -0500

    Update LonghornHapkit.ino

--- LonghornHapkit/LonghornHapkit.ino
+++ LonghornHapkit/LonghornHapkit.ino
@@ -7,15 +7,16 @@
 
 // INCLUDES
 #define ENCODER_OPTIMIZE_INTERRUPTS
-#include <TimerOne.h>  // This library manages the timing of the haptic loop 
+#include <TimerOne.h>  // This library manages the timing of the haptic loop
 #include <Encoder.h>   // This library manages the encoder read.
 
 //enable select  functions
 //#define ItsWallTime
 //#define ItsDampingTime
-#define ItsFrictionTime
+//#define ItsFrictionTime
 //#define ItsBumpTime
-//#define ItsTextureTime
+#define ItsTextureTime
+//#define ItsSurfaceTime
 
 // Pin Declarations
 const int PWMoutp = 4;
@@ -28,10 +29,10 @@ const int encoder0PinB = 3;
 Encoder encoder(encoder0PinA,encoder0PinB);
 
 double encoderResolution = 48;
-double pos = 0; 
-double lastPos = 0; 
+double pos = 0;
+double lastPos = 0;
 double lastVel = 0;
-double vh = 0; 
+double vh = 0;
 double lastXh = 0;
 double lastLastVh = 0;
 double lastVh = 0;
@@ -52,9 +53,9 @@ double dxh_filt_prev2;
 // *******************************************
 // UNCOMMENT THESE AND INCLUDE CORRECT NUMBERS
 // *******************************************
-double rh = 0.069;   //[m] 
-double rp = 0.005;  //[m] 
-double rs = 0.074;  //[m] 
+double rh = 0.069;   //[m]
+double rp = 0.005;  //[m]
+double rs = 0.074;  //[m]
 // *******************************************
 
 // Force output variables
@@ -63,8 +64,14 @@ double Tp;              // torque of the motor pulley
 unsigned int output = 0;    // output command to the motor
 
 // Timing Variables: Initalize Timer and Set Haptic Loop
-boolean hapticLoopFlagOut = false; 
-boolean timeoutOccured = false; 
+boolean hapticLoopFlagOut = false;
+boolean timeoutOccured = false;
+
+unsigned long t = 0; // time since program started
+unsigned long t_imp = 0; //time wall impact occurred
+boolean inWall = false;
+boolean impact = false;
+
 
 //--------------------------------------------------------------------------
 // Initialize
@@ -80,9 +87,9 @@ void setup()
  pinMode(PWMspeed, OUTPUT);
 
  // Haptic Loop Timer Initalization
-   Timer1.initialize(); 
-  long period = 1000; // [us]  10000 [us] - 100 Hz 
-  Timer1.attachInterrupt(hapticLoop,period); 
+   Timer1.initialize();
+  long period = 1000; // [us]  10000 [us] - 100 Hz
+  Timer1.attachInterrupt(hapticLoop,period);
 
   // Init Position and Velocity
   lastPos = encoder.read();
@@ -92,7 +99,7 @@ void setup()
   digitalWrite(PWMoutp, HIGH);
   digitalWrite(PWMoutn, LOW);
   analogWrite(PWMspeed, 0);
-  
+
 }
 
 //--------------------------------------------------------------------------
@@ -113,13 +120,13 @@ void loop()
   void hapticLoop()
   {
 
-      // See if flag is out (couldn't finish before another call) 
+      // See if flag is out (couldn't finish before another call)
       if(hapticLoopFlagOut)
       {
         timeoutOccured = true;
       }
       //*************************************************************
-      //*** Section 1. Compute position and velocity using encoder (DO NOT CHANGE!!) ***  
+      //*** Section 1. Compute position and velocity using encoder (DO NOT CHANGE!!) ***
       //*************************************************************
       pos = encoder.read();
       double vel = (.80)*lastVel + (.20)*(pos - lastPos)/(.01);
@@ -128,33 +135,33 @@ void loop()
         //*************************************************************
         //*** Section 2. Compute handle position in meters ************
         //*************************************************************
-      
+
           // ADD YOUR CODE HERE
 
           // SOLUTION:
           // Define kinematic parameters you may need
-           
+
           // Step 2.1: print updatedPos via serial monitor
           //*************************************************************
 
            //Serial.println(pos);
-           
+
           // Step 2.2: Compute the angle of the sector pulley (ts) in degrees based on updatedPos
          //*************************************************************
 
             double ts = -pos/48*360*rp/rs; // NOTE - THESE NUMBERS MIGHT NOT BE CORRECT! USE KINEMATICS TO FIGRUE IT OUT!
-           
-       
+
+
          // Step 2.3: Compute the position of the handle based on ts
           //*************************************************************
 
-            xh = rh*(ts*3.14159/180);       // Again, these numbers may not be correct. You need to determine these relationships. 
-         
+            xh = rh*(ts*3.14159/180);       // Again, these numbers may not be correct. You need to determine these relationships.
+
           // Step 2.4: print xh via serial monitor
           //*************************************************************
 
-           //Serial.println(xh,5);
-           
+           Serial.println(xh,5);
+
           // Step 2.5: compute handle velocity
           //*************************************************************
              vh = -(.95*.95)*lastLastVh + 2*.95*lastVh + (1-.95)*(1-.95)*(xh-lastXh)/.0001;  // filtered velocity (2nd-order filter)
@@ -163,32 +170,32 @@ void loop()
              lastVh = vh;
 
         //*************************************************************
-        //*** Section 3. Assign a motor output force in Newtons *******  
+        //*** Section 3. Assign a motor output force in Newtons *******
         //*************************************************************
- 
-            // Init force 
+
+            // Init force
             double force = 0;
             //double K = 300;  // spring stiffness
 
-            //force = K*xh; 
+            //force = K*xh;
 
          // This is just a simple example of a haptic wall that only uses encoder position.
-         // You will need to add the rest of the following cases. You will want to enable some way to select each case. 
-         // Options for this are #DEFINE statements, swtich case statements (i.e., like a key press in serial monitor), or 
-         // some other method. 
-          
-          // Virtual Wall 
+         // You will need to add the rest of the following cases. You will want to enable some way to select each case.
+         // Options for this are #DEFINE statements, swtich case statements (i.e., like a key press in serial monitor), or
+         // some other method.
+
+          // Virtual Wall
         //*************************************************************
            #if defined(ItsWallTime)
            double K=300;
              if(xh > 0.005){
-              force = K*(xh-0.005); 
+              force = K*(xh-0.005);
              }else{
               force = 0;
              }
            #endif
-       
-         // Linear Damping 
+
+         // Linear Damping
         //*************************************************************
            #if defined(ItsDampingTime)
            double b = 1;
@@ -199,8 +206,13 @@ void loop()
          // I want to try Brown and McPhee continuous non-linear friciton model
         //*************************************************************
            #if defined(ItsFrictionTime)
+<<<<<<< HEAD
            
            double F_C=0;    //coulombic friction
+=======
+
+           double F_C=.3;    //coulombic friction
+>>>>>>> de3799d6e281e5dbcb06e5def380b1cfada05bab
            double F_S=.5;     //static friction
            double v_S=0.06;  //stribeck velocity
            double v_T=vh;    //tangential velocity
@@ -211,19 +223,55 @@ void loop()
            }else if(abs(v_T)<0.0001){
             //force=.3*vh/abs(vh);
            }else{
+<<<<<<< HEAD
             //b=0.1;
             //force=b*vh;
             force=((F_C*tanh(4*abs(v_T)/v_S)+(F_S-F_C)*(abs(v_T)/v_S)/pow((.25*pow((abs(v_T)/v_S),2)+.75),2)))*v_T/abs(v_T);
            }     
+=======
+            //b=b;
+            force=((F_C*tanh(4*abs(v_T)/v_S)+(F_S-F_C)*(abs(v_T)/v_S)/pow((.25*pow((abs(v_T)/v_S),2)+.75),2)))*v_T/abs(v_T);
+           }
+           force=b*vh;
+>>>>>>> de3799d6e281e5dbcb06e5def380b1cfada05bab
            #endif
 
-         // A Hard Surface 
+
+         // A Hard Surface
         //*************************************************************
            #if defined(ItsSurfaceTime)
             //not sure about this one. force needs to be a function of time?
+           double d = 500;  //[ms]
+           double f = 50;   //[rad/s]
+           double A = 0.001; //[N/(m/s)]
+           t = millis();    //[ms]
+
+           if(xh<0){
+            force = 0;
+              inWall = false;
+           }
+           else{
+            force = -K*xh;
+            if(!inWall&&!impact){
+              impact = true;
+              t_imp = t;
+            }
+            inWall = true;
+            Serial.println("Hit");
+           }
+
+//           if(impact){
+//            //force = force+A*abs(vh)*exp(log(0.001)*(t-t_imp)/d)*sin(f*(t-t_imp)/1000);
+//            if(t-t_imp>d){
+//              impact = false;
+//            }
+//           }
+
+           Serial.println(force);
            #endif
 
-         // Bump and Valley  
+
+         // Bump and Valley
         //*************************************************************
            #if defined(ItsBumpTime)
            double fq=1000;   // frequency adjuster
@@ -231,7 +279,7 @@ void loop()
            force=amp*sin(fq*xh);
            #endif
 
-          // Texture 
+          // Texture
         //*************************************************************
            #if defined(ItsTextureTime)
            //I just copied the bump code. Make some changes to get an interesting result
@@ -239,30 +287,30 @@ void loop()
            double amp=.3; // amplitude adjuster
            force=amp*sin(fq*xh);
            #endif
-           
-           // CHALLENGE POINTS: Try simulating a paddle ball! Hint you need to keep track of the virtual balls dynamics and 
-           // compute interaction forces relative to the changing ball position.  
+
+           // CHALLENGE POINTS: Try simulating a paddle ball! Hint you need to keep track of the virtual balls dynamics and
+           // compute interaction forces relative to the changing ball position.
         //*************************************************************
-        
- 
+
+
 
       //*************************************************************
       //*** Section 5. Force output (do not change) *****************
       //*************************************************************
-       
-        // Determine correct direction 
+
+        // Determine correct direction
         //*************************************************************
         double Tp = force*rh*rp/rs;
         if(Tp < 0)
         {
         digitalWrite(PWMoutp, HIGH);
         digitalWrite(PWMoutn, LOW);
-        } else 
+        } else
         {
         digitalWrite(PWMoutp, LOW);
         digitalWrite(PWMoutn, HIGH);
-        } 
-        
+        }
+
         // Convert Torque to Duty
         int duty=abs(Tp)*255/0.008;
         // Limit torque to motor and write
@@ -278,11 +326,11 @@ void loop()
             Serial.println(force); // Could print this to troublshoot but don't leave it due to bogging down speed
         // Write out the motor speed.
         //*************************************************************
-        //setPwmFrequency(9, 8);   
+        //setPwmFrequency(9, 8);
         analogWrite(PWMspeed, duty); //abs(force)
-  
-  // Update variables 
+
+  // Update variables
   lastVel = vel;
-  lastPos = pos; 
+  lastPos = pos;
 
 }
